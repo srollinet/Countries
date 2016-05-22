ce119c9c3e597911e4ce3a0575220bf7f253be2d
Sylvain <sylvain.rollinet@gmail.com>
2016-05-22 21:40:14 +0200

    Add project files.

---
+++ src/SRoll.Countries.Test/CountriesTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Threading;
+using Xunit;
+
+namespace SRoll.Countries.Test
+{
+    public class CountriesTest
+    {
+        public CountriesTest()
+        {
+            CultureInfo.CurrentUICulture = new CultureInfo("en");
+        }
+
+        [Fact]
+        public void CreateTest()
+        {
+            var country = new Country("CH", "CHE", "");
+            Assert.Equal("Switzerland", country.Name);
+        }
+
+        [Fact]
+        public void OtherCultureTest()
+        {
+            CultureInfo.CurrentUICulture = new CultureInfo("fr");
+            var country = new Country("CH", "CHE", "");
+            Assert.Equal("Suisse", country.Name);
+        }
+
+        [Fact]
+        public void GetListTest()
+        {
+            var list = Country.Countries;
+        }
+    }
+}

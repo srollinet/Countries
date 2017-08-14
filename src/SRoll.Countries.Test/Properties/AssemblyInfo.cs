a65d0167e923518eb67ac2038c9b2051ef929991
Sylvain <sylvain.rollinet@gmail.com>
2017-08-14 16:42:36 +0200

    Migrate to csproj projects

--- src/SRoll.Countries.Test/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SRoll.Countries.Test")]
-[assembly: AssemblyProduct("SRoll.Countries")]
-
-[assembly: AssemblyCompany("SRoll @ https://github.com/srollinet")]
-[assembly: AssemblyCopyright("Copyright © SRoll 2016")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
--- src/SRoll.Countries/Properties/AssemblyInfo.cs
+++ src/SRoll.Countries/Properties/AssemblyInfo.cs
@@ -1,17 +1,5 @@
-﻿using System.Reflection;
-using System.Resources;
+﻿using System.Resources;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("SRoll.Countries")]
-[assembly: AssemblyProduct("SRoll.Countries")]
-
-[assembly: AssemblyCompany("SRoll @ https://github.com/srollinet")]
-[assembly: AssemblyCopyright("Copyright © SRoll 2016")]
-
-[assembly:AssemblyDescription("Localized list of ISO-3166 country codes.")]
-
-[assembly: NeutralResourcesLanguage("en")]
-
 [assembly: InternalsVisibleTo("SRoll.Countries.Test")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
